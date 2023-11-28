1e25e1a5c59c096e910b690a8f5f04c01e86846f
Hailemichael Atrsaw <hailat.alx@gmail.com>
2023-11-28 14:17:16 +0300

    all done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { APIResponse, Game } from 'src/app/models';
 import { HttpService } from 'src/app/services/http.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,12 +14,11 @@ export class HomeComponent implements OnInit, OnDestroy {
   public games: Array<Game>;
   private routeSub: Subscription;
   private gameSub: Subscription;
-  
-  constructor(
+
+  constructor(    
     private httpService: HttpService,
     private router: Router,
-    private activatedRoute: ActivatedRoute
-  ) { }
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
@@ -52,4 +52,5 @@ export class HomeComponent implements OnInit, OnDestroy {
       this.routeSub.unsubscribe();
     }
   }
+
 }
